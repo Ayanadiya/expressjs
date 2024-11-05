a13610c799bc46d6dd506034851f669687f031ba
Ayanadiya <ayanadiya17@gmail.com>
2024-11-05 11:53:46 +0530

    add-product route is added

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser =require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/add-product',(req,res,next) =>{
+    res.send(`
+        <h1>Add Product Page</h1>
+        <form action="/product" method="POST">
+          <label for="name">Title</label>
+          <input type="text" name="title" id="name">
+          <label for="size">Size</label>
+          <input type="text" name="size" id="size">
+          <button type="submit">Add Product</button>
+        </form>
+        `)
 })
 
-app.use((req,res,next) => {
-    console.log('In another middleware');
+app.post('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next) => {
     res.send('<h1>Hello from Express.js</h1>');
 })
 
